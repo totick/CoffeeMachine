2928d50883d0d9e568d6892fedd53c5dba647de0
John Lundgren <john.lundgren.lexicon@gmail.com>
2026-02-01 07:12:59 +0100

    Turn off coffee machine

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,23 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+while True:
+    print("What would you like? (espresso/latte/cappuccino)")
+    user_input = input(">> ").strip().lower()
+
+    match user_input:
+        case "off":
+            break
+        case "espresso" | "latte" | "cappuccino":
+            #prepare_drink(user_input)
+            pass
+        case "report":
+            #print_report()
+            pass
+        case _:
+            #print(user_input)
+            pass
+
+print("Coffee machine off.")
